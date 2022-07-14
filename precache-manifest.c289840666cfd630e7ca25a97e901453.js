9efcec8f02beef6271d2c146c462eedd0e630a5f
deibo <deib0@foxmail.com>
2022-07-14 15:40:22 +0800

    deploy

---
+++ precache-manifest.c289840666cfd630e7ca25a97e901453.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54347a1dd6a55a6b4d901753de2d51c4",
+    "url": "/money-react-website/index.html"
+  },
+  {
+    "revision": "ef17730914c95826d799",
+    "url": "/money-react-website/static/css/main.7170edac.chunk.css"
+  },
+  {
+    "revision": "6a6e7fb7fe24b2086cc9",
+    "url": "/money-react-website/static/js/2.0e1ada90.chunk.js"
+  },
+  {
+    "revision": "de7c95d42723c2d4f76d99ad9a65a751",
+    "url": "/money-react-website/static/js/2.0e1ada90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef17730914c95826d799",
+    "url": "/money-react-website/static/js/main.c126c405.chunk.js"
+  },
+  {
+    "revision": "85563d754b0ec5d98e71",
+    "url": "/money-react-website/static/js/runtime-main.56c39c0f.js"
+  }
+]);
